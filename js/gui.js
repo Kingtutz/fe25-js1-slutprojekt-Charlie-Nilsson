9258b05c1e0009e9ddec3f88c78137bae2c20628
Kingtutz <challe-1992@hotmail.com>
2025-12-13 22:37:06 +0100

    pics added

--- js/gui.js
+++ js/gui.js
@@ -1,7 +1,7 @@
 import { selectedValue } from './index.js'
 
 const topListDisplay = document.querySelector('#toplistdisplaybox')
-
+const imgUrl = `https://image.tmdb.org/t/p/original`
 async function renderTopList (data) {
   console.log(data)
   console.log('selectvalueinrender', selectedValue)
@@ -13,19 +13,20 @@ async function renderTopList (data) {
       topListDisplay.append(h3)
       topListDisplay.append(pEl)
       h3.innerText = result.name
-      pEl.innerText = "First air day: " +result.first_air_date
-
-
+      pEl.innerText = 'First air day: ' + result.first_air_date
     })
   } else {
     results.forEach(result => {
       const h5 = document.createElement('h5')
       const pEl = document.createElement('p')
+      const img = document.createElement('img')
       topListDisplay.append(h5)
+      topListDisplay.append(img)
       topListDisplay.append(pEl)
       h5.innerText = result.title
-      pEl.innerText = "Realese date: "+result.release_date
-
+      pEl.innerText = 'Realese date: ' + result.release_date
+      img.src = imgUrl + result.poster_path
+      img.classList.add('poster')
     })
   }
 }
